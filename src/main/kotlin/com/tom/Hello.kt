d6911c14f3e5234f3c0feedd027eaa6f6de1e71b
Wei Chen <aroma.jonathan@gmail.com>
2018-12-25 15:15:19 +0800

    java and kotlin variable, data type

--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -5,6 +5,15 @@ fun main(args: Array<String>) {
 //    Human().hello()
     val h=Human()
     h.hello()
+
+    var age = 19
+    age = 20
+    var weight = 66.5f
+
+    var name:String
+    name = "Jonathan"
+
+
 }
 
 class Human{
